58458fc8b8781f4f06cb2df8951e00076fb6a8ee
DISHEN <dishenmakwana.dm@gmail.com>
2022-07-05 17:26:11 +0530

    ADD: error handling function & CHG: k8s file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,25 @@
 import express, { json } from 'express';
+import 'express-async-errors';
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/signin';
+import { signOutRouter } from './routes/signout';
+import { signUpRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
-
 app.use(json());
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.all('*', async (req, res, next) => {
+  next(new NotFoundError());
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Auth Listening on port 3000');
