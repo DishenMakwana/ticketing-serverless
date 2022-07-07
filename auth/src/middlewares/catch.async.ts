4c17e523e22dd8f24253f00c5aea8939581ec4c9
DISHEN <dishenmakwana.dm@gmail.com>
2022-07-07 18:23:01 +0530

    ADD: current-user API

--- auth/src/middlewares/catch.async.ts
+++ auth/src/middlewares/catch.async.ts
@@ -2,7 +2,10 @@ import { Request, Response, NextFunction } from 'express';
 
 const catchAsync =
   (fn: Function) => (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch((error) => next(error));
+    Promise.resolve(fn(req, res, next)).catch((error) => {
+      console.log('CatchAsync err:', error);
+      next(error);
+    });
   };
 
 export default catchAsync;
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,20 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import catchAsync from '../middlewares/catch.async';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('current user');
-});
+router.get(
+  '/api/users/currentuser',
+  catchAsync((req: Request, res: Response) => {
+    if (!req.session?.jwt) {
+      return res.send({ currentUser: null });
+    }
+
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+
+    res.send({ currentUser: payload });
+  })
+);
 
 export { router as currentUserRouter };
