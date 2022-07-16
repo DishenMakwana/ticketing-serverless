0c49c98720622ab0c3e8fa84fe2374379d2aa706
DISHEN <dishenmakwana.dm@gmail.com>
2022-07-16 16:00:48 +0530

    ADD: nats service in ticketing service

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
   requireAuth,
   NotAuthorizedError,
 } from '@dishen/common';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -38,6 +40,13 @@ router.put(
     });
 
     await ticket.save();
+
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
   }
