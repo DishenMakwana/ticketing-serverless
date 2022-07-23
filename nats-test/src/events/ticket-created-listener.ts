05a25c2030a5768cb1493751733ff1d7c2c94180
DISHEN <dishenmakwana.dm@gmail.com>
2022-07-23 14:13:00 +0530

    ADD: changes in infra and nats-test module

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -4,7 +4,7 @@ import { TicketCreatedEvent } from './../events/ticket-created-event';
 import { Subjects } from './../events/subjects';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
-  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  readonly subject = Subjects.TicketCreated;
   queueGroupName = 'payments-service';
 
   onMessage(data: TicketCreatedEvent['data'], msg: Message) {
